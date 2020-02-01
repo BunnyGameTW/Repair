753c659950a8a9788623f6283aba9c398f9bf3cd
DESKTOP-K8JBJVJ\karry <karry9527100@gmail.com>
2020-02-01 16:16:38 +0800

    Add loading scene

---
+++ Assets/Script/DragDetector.cs
@@ -0,0 +1,85 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
+
+public class DragDetector : MonoBehaviour, IBeginDragHandler, IDragHandler, IEndDragHandler
+{
+    public int scale = 100;
+
+    public bool IsDragable;
+
+    [SerializeField]
+    private Scrollbar scrollbar;
+    private float scrollbarSize;
+    private float mouseX;
+    private Vector3 screenPoint;
+    private Vector3 offset;
+
+    void IBeginDragHandler.OnBeginDrag(PointerEventData eventData)
+    {
+        Debug.Log("OnBeginDrag");
+
+        if (IsDragable)    // Only do if IsDraggable == true
+        {
+            mouseX = Input.mousePosition.x;
+            scrollbarSize = scrollbar.size;
+            //screenPoint = Camera.main.WorldToScreenPoint(gameObject.transform.position);
+
+            //offset = gameObject.transform.position - Camera.main.ScreenToWorldPoint(new Vector3(Input.mousePosition.x, Input.mousePosition.y, screenPoint.z));
+        }
+    }
+
+    public void OnDrag(PointerEventData eventData)
+    {
+        Debug.Log("OnDrag");
+
+        if (IsDragable)    // Only do if IsDraggable == true
+        {
+            float offset = (Input.mousePosition.x - mouseX) / scale;
+            scrollbar.size = scrollbarSize + offset;
+            //Vector3 curScreenPoint = new Vector3(Input.mousePosition.x, Input.mousePosition.y, screenPoint.z); // hardcode the y and z for your use
+
+            //Vector3 curPosition = Camera.main.ScreenToWorldPoint(curScreenPoint) + offset;
+            //transform.position = curPosition;
+        }
+    }
+
+    /*void OnMouseDown()
+    {
+        Debug.Log("OnMouseDown");
+
+        if (IsDragable)    // Only do if IsDraggable == true
+        {
+            mouseX = Input.mousePosition.x;
+            scrollbarSize = scrollbar.size;
+            //screenPoint = Camera.main.WorldToScreenPoint(gameObject.transform.position);
+
+            //offset = gameObject.transform.position - Camera.main.ScreenToWorldPoint(new Vector3(Input.mousePosition.x, Input.mousePosition.y, screenPoint.z));
+        }
+    }
+
+    void OnMouseDrag()
+    {
+        Debug.Log("OnMouseDrag");
+        if (IsDragable)    // Only do if IsDraggable == true
+        {
+            float offset = (Input.mousePosition.x - mouseX) / 100;
+            scrollbar.size = scrollbarSize + offset;
+            //Vector3 curScreenPoint = new Vector3(Input.mousePosition.x, Input.mousePosition.y, screenPoint.z); // hardcode the y and z for your use
+
+            //Vector3 curPosition = Camera.main.ScreenToWorldPoint(curScreenPoint) + offset;
+            //transform.position = curPosition;
+        }
+    }*/
+
+
+
+
+
+
+    public void OnEndDrag(PointerEventData eventData)
+    {
+    }
+}
--- Assets/Script/LoadingManager.cs
+++ Assets/Script/LoadingManager.cs
@@ -1,18 +1,67 @@
-﻿using System.Collections;
+﻿using DG.Tweening;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class LoadingManager : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    [SerializeField]
+    private Scrollbar scrollbar;
+    [SerializeField]
+    private Image image;
+    [SerializeField]
+    private Text text;
+    [SerializeField]
+    private Text loadingText;
+    [SerializeField]
+    private DragDetector dragDetector;
+    [SerializeField]
+    private Image light;
 
-    // Update is called once per frame
-    void Update()
+    private float lightDuration = 1.0f;
+    private Vector3 imagePos;
+    Sequence sequence;
+    Sequence lightSequence;
+
+    private void Start()
     {
-        
+        imagePos = image.transform.localPosition;
+        scrollbar.interactable = false;
+        text.transform.DOLocalMoveY(650, 10).SetEase(Ease.Linear);
+
+        // 0%: -483, 87%: 476.61, 100%: 620
+        image.transform.DOLocalMoveX(476.61f, 10).SetEase(Ease.Linear);
+        sequence = DOTween.Sequence();
+
+        sequence.Append(DOTween.To(() => scrollbar.size, x => scrollbar.size = x, 0.87f, 10).SetEase(Ease.Linear)).OnComplete(() =>
+        {
+            dragDetector.IsDragable = true;
+        });
+        sequence.AppendCallback(() =>
+        {
+            Color startColor = new Color(1, 1, 1, 1);
+            Color endColor = new Color(1, 1, 1, 0);
+
+            Sequence lightSequence = DOTween.Sequence();
+            lightSequence.AppendCallback(() =>
+            {
+                light.color = endColor;
+                light.DOColor(startColor, lightDuration / 2).OnComplete(() => { light.DOColor(endColor, lightDuration / 2); });
+
+            });
+            lightSequence.AppendInterval(lightDuration);
+            lightSequence.SetLoops(-1);
+        });
+
+    }
+    private void Update()
+    {
+        loadingText.text = "Loading..." + (scrollbar.size * 100).ToString("N0") + "%";
+
+        Vector3 pos = new Vector3(-483 + scrollbar.size * 1103, imagePos.y, imagePos.z);
+        Vector3 lightPos = new Vector3(-470 + scrollbar.size * 1103, -429, 0);
+        image.rectTransform.localPosition = pos;
+        light.transform.localPosition = lightPos;
     }
 }
