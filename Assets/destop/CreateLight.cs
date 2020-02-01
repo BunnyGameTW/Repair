1b984c9d1f1ae7371c49f2f5d16e9ff1d03b7b96
ZigerMa <zigerma@gmail.com>
2020-02-01 18:38:47 +0800

    push flashlight

--- Assets/destop/CreateLight.cs
+++ Assets/destop/CreateLight.cs
@@ -2,4 +2,34 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+public class CreateLight : MonoBehaviour
+{
+    public GameObject flashlight; //宣告物件，名稱platform1
+    public bool isFlashLightExist = false;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+    
+    }
+
+    public void CreateFlashLight()
+    {
+        if (isFlashLightExist == false)
+        {
+            isFlashLightExist = true;
+            Vector3 pos = new Vector3(0f, -7f, -60);
+            Instantiate(flashlight, pos, transform.rotation);
+            Debug.Log("456");
+        }
+
+    }
+
+
+}
 
