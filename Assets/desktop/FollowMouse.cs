93b42b60bbcc273fcae956048559cd0567141290
ZigerMa <zigerma@gmail.com>
2020-02-01 20:26:29 +0800

    rename folder And Follow Mouse

---
+++ Assets/desktop/FollowMouse.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FollowMouse : MonoBehaviour
+{
+    public bool isFollow = false;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (isFollow == true)
+        {
+            Vector3 pos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+            gameObject.transform.position = Camera.main.ScreenToWorldPoint(new Vector3(Input.mousePosition.x, Input.mousePosition.y, 60));
+        }
+
+    }
+    public void follow()
+    {
+        isFollow = true;
+
+    }
+}
