7b37cb11045937f504b78c4f2a2680b60de26349
ZigerMa <zigerma@gmail.com>
2020-02-02 11:49:16 +0800

    fixed SceneManager

--- Assets/desktop/FollowMouse.cs
+++ Assets/desktop/FollowMouse.cs
@@ -8,7 +8,7 @@ public class FollowMouse : MonoBehaviour
     public GameObject aa;
     public bool isFollow = false;
     public bool isShowScrew = false;
-    public bool isGetKeyS = StartGameManager.Singleton.GetHasFixLetter("s");
+    public bool isGetKeyT = StartGameManager.Singleton.GetHasLetter("t");
 
 
     // Start is called before the first frame update
@@ -24,17 +24,17 @@ public class FollowMouse : MonoBehaviour
         {
             Vector3 pos = Camera.main.ScreenToWorldPoint(new Vector3(Input.mousePosition.x, Input.mousePosition.y, 60));
             gameObject.transform.position = pos;
-            if (Mathf.Abs(pos.x - 23) < 10 && Mathf.Abs(pos.y + 34) < 10 && isGetKeyS == false)
+            if (Mathf.Abs(pos.x + 43) < 10 && Mathf.Abs(pos.y + 34) < 10 && isGetKeyT == false)
             {
                 screwdriver.SetActive(true);
                 isShowScrew = true;
-                StartGameManager.Singleton.SetHasFixLetter("s");
+                StartGameManager.Singleton.SetHasLetter("t");
             }
             if (isShowScrew)
             {
                 StartCoroutine(hide());
                 float step = 50 * Time.deltaTime;
-                screwdriver.transform.localPosition = Vector3.MoveTowards(screwdriver.transform.localPosition, new Vector3(-278, 347, 50), step);
+                screwdriver.transform.localPosition = Vector3.MoveTowards(screwdriver.transform.localPosition, new Vector3(198, 441, 50), step);
             }
         }
     }
--- Assets/desktop/SceneSwitcher.cs
+++ Assets/desktop/SceneSwitcher.cs
@@ -8,10 +8,14 @@ public class SceneSwitcher : MonoBehaviour
     public bool  isGetKeyS = StartGameManager.Singleton.GetHasFixLetter("s");
     void Start()
     {
-       if (isGetKeyS == true)
-       {
+
+    }
+    void Update()
+    {
+        if (isGetKeyS == true)
+        {
             MiniGameBtn.SetActive(false);
-       }
+        }
     }
     public void switchLobby()
     {
